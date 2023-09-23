a56cb14acaf0869c86d8fc1f695ae16dc96df232
Maciej Grula <maciej.grula@execon.pl>
2023-09-23 10:31:55 +0200

    quit when all windows closed

--- main.js
+++ main.js
@@ -57,7 +57,9 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  // if (process.platform !== 'darwin') app.quit()
+  // when all windows closed quit the app even on macos
+  app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
